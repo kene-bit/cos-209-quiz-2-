497bc0ce5cfa4a4fc5abf9819c1a4ed2884b5304
kene-bit <princekene80@gmail.com>
2025-11-18 09:53:19 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to App 1</h1>
+        <p>This is a minimal scaffold created without running npm install.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
